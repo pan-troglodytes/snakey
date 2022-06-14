ccc3421e5afe75042cc3cd487aea2caa9c0b57d5
sacha <pantroggy@protonmail.com>
2022-06-14 01:10:01 +0100

    ItemSpawner now only spawns items where there is space for them

--- src/ItemSpawner.java
+++ src/ItemSpawner.java
@@ -3,15 +3,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ItemSpawner implements ActionListener {
     private static Blueprint map;
     private ArrayList<Item> items = new ArrayList<>();
     private int itemsMax = 3;
-    private int col;
-    private int row;
-
     private int[] coordsTopLeft;
     private int[] coordsBotRight;
 
@@ -19,25 +16,31 @@ public class ItemSpawner implements ActionListener {
         map = b;
     }
     ItemSpawner(int x1, int y1, int x2, int y2 ) {
-        new Timer(1500, this).start();
+        new Timer(1000, this).start();
         coordsTopLeft = new int[] {x1,y1};
         coordsBotRight = new int[] {x2,y2};
-        col = map.xy.size();
-        row = map.xy.get(0).size();
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         if (items.size() < itemsMax) {
             Item itemNew = new Apple(this);
-            int[] newPos = new int[2];
-            do {
-                newPos[0] = ThreadLocalRandom.current().nextInt(coordsTopLeft[0], coordsTopLeft[1]);
-                newPos[1] = ThreadLocalRandom.current().nextInt(coordsBotRight[0], coordsBotRight[1]);
-            } while (map.getCoords(newPos[0], newPos[1]).iterator().hasNext() && map.getCoords(newPos[0], newPos[1]).iterator().next().equals(Snake.class));
-            itemNew.setPosition(newPos[0], newPos[1]);
-            items.add(itemNew);
+            ArrayList<Integer> newPos;
+            ArrayList<ArrayList<Integer>> freeSpace = new ArrayList<>();
+            for (int i=coordsTopLeft[0]; i <= coordsBotRight[0]; i++) {
+                for (int j=coordsTopLeft[1]; j <= coordsBotRight[1]; j++) {
+                    if (map.getCoords(i,j).size() == 0) {
+                        freeSpace.add(new ArrayList<>());
+                        freeSpace.get(freeSpace.size()-1).add(i);
+                        freeSpace.get(freeSpace.size()-1).add(j);
+                    }
+                }
+            }
+            if (freeSpace.size() > 0) {
+                newPos = freeSpace.get(new Random().nextInt(freeSpace.size()));
+                itemNew.setPosition(newPos.get(0), newPos.get(1));
+                items.add(itemNew);
+            }
         }
-
     }
 
     public void drawItems(Graphics g, int tileSize) {
